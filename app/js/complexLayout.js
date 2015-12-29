bc61edbc3c64c95b52ae4f43814ed624d06f77ad
smileLuckyBoy <1044303587@qq.com>
2015-12-29 08:20:24 +0800

    add distributed

--- app/js/complexLayout.js
+++ app/js/complexLayout.js
@@ -3,10 +3,10 @@ Ext.onReady(function() {
     Ext.QuickTips.init();
 
     //init layout objects
-    var  layoutExamples = [];
+    var layoutExamples = [];
 
     var basicLayoutObj = getBasicLayout();
-    Ext.Object.each(basicLayoutObj,function(key,value,myselef){
+    Ext.Object.each(basicLayoutObj, function(key, value, myselef) {
         layoutExamples.push(value);
     });
 
@@ -29,25 +29,35 @@ Ext.onReady(function() {
         store: store
     });
 
+    var detailEl;
     //select listener
-    treePanel.getSelectionModel().on('select',function(sm,record){
-        if(record.get('leaf')){
+    treePanel.getSelectionModel().on('select', function(sm, record) {
+        if (record.get('leaf')) {
             var key = record.get('id');
-            if(basicLayoutObj[key] !== null){
-                Ext.getCmp('content-panel').layout.setActiveItem(key+'-panel');
+            if (basicLayoutObj[key] !== null) {
+                Ext.getCmp('content-panel').layout.setActiveItem(key + '-panel');
             }
+
+            if (!detailEl) {
+                var bd = Ext.getCmp('detail-panel').body;
+                bd.update('').setStyle('background', '#FFFFF');
+                detailEl = bd.createChild();
+            }
+            detailEl.hide().update(Ext.getDom(record.getId() + '-detail').innerHTML).slideIn('l', {
+                duration: 200
+            });
         }
 
     });
 
     //center panel
     var contentPanel = {
-        id : 'content-panel',
-        region : 'center',
-        layout : 'card',
-        activeItem : 0,
-        border : false,
-        items : layoutExamples
+        id: 'content-panel',
+        region: 'center',
+        layout: 'card',
+        activeItem: 0,
+        border: false,
+        items: layoutExamples
     };
 
     //build the main layout with the viewport
@@ -55,32 +65,40 @@ Ext.onReady(function() {
         id: 'main-panel',
         layout: 'border',
         items: [{
-            region: 'west',
-            id: 'west-panel',
-            title: 'west',
-            split: true,
-            collapsible: true,
-            animCollapse: true,
-            width: 200,
-            minWidth: 175,
-            maxWidth: 250,
-            layout: 'accordion',
-            items: [{
-                layout: 'fit',
-                title: 'layout browser',
-                iconCls: 'info',
-                items: [treePanel]
-            }, {
-                title: 'Navigation',
-                iconCls: 'nav'
-            }, {
-                title: 'settings',
-                iconCls: 'settings',
-                html: '<p>some settings here</p>'
-            }]
+                region: 'west',
+                id: 'west-panel',
+                title: 'west',
+                split: true,
+                collapsible: true,
+                animCollapse: true,
+                width: 200,
+                minWidth: 175,
+                maxWidth: 250,
+                layout: 'accordion',
+                items: [{
+                    layout: 'fit',
+                    title: 'layout browser',
+                    iconCls: 'info',
+                    items: [treePanel]
+                }, {
+                    title: 'Navigation',
+                    iconCls: 'nav'
+                }, {
+                    title: 'settings',
+                    iconCls: 'settings',
+                    html: '<p>some settings here</p>'
+                }]
 
-        }, 
-            contentPanel
+            },
+            contentPanel, {
+                region: 'east',
+                id: 'detail-panel',
+                title: 'Code Preview',
+                split: true,
+                collapsible: true,
+                animCollapse: true,
+                width: 200
+            }
         ]
     });
 });
